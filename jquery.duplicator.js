51afaa6d80de7a462d7cc88c095fbed3ed62272e
Ben Edmunds <ben.edmunds@gmail.com>
2011-05-18 22:57:51 -0500

    Added templating.  Modified to support form element arrays.

--- jquery.duplicator.js
+++ jquery.duplicator.js
@@ -3,11 +3,14 @@
   $.fn.duplicator = function( options ) {
 
     var settings = {
+      'rowSelector'    : '',                // selector for "row" to duplicate
       'addSelector'    : '',                // selector for "add" button
       'removeSelector' : '',                // selector of generic "remove" button
       'removeMarkup'   : '',                // markup of remove button
       'slideEffect'    : false,             // use sliding effect for adding/removing
       'maxDuplicates'  : 0,                 // set max number of duplications
+      'templateClass'  : '',                // set max number of duplications
+	  'templateKey'    : '[0]',              //replace original form element keys (if not template)
       limit            : function(limit) {} // callback for limiting out duplications
     };
 
@@ -17,25 +20,36 @@
     if ( options ) {
         $.extend( settings, options );
     }
+	
+	
+	var original = $self.find(settings.rowSelector);
+	
+	//change the original keys if this is not a template
+	if (settings.templateClass != '') {
+		settings.templateKey = '[]';
+	}
+	else {
+		original.html(original.html().replaceAll('[]', settings.templateKey));
+	}
+	
     
     $(settings.removeSelector).live('click', function() {
 
-        var parent = $(this).parent();
-        var current_count = $.data(self,'duplicates');
-        $.data(self, 'duplicates', current_count - 1);
-        if ( settings.slideEffect ) {
+        var parents = $(this).parents(settings.rowSelector);
+
+		if ( settings.slideEffect ) {
             
-            parent.fadeTo('fast', 0.01, function() {
-                parent.slideUp(function(){
-                    parent.remove();
+            parents.fadeTo('fast', 0.01, function() {
+                parents.slideUp(function(){
+                    parents.remove();
                 });
             });
             
         }
         else {
             
-            parent.fadeOut(function() {
-                parent.remove();
+            parents.fadeOut(function() {
+                parents.remove();
             });
             
         }
@@ -46,39 +60,50 @@
 
     $(settings.addSelector).click(function() {
 
-        var duplicates     = 0;
-        var new_element    = $self.clone();
+		//count total duplicates
+		var duplicates = 0;
+		if (settings.templateClass != '') {
+			duplicates = $self.find(settings.rowSelector +':not(.'+ settings.templateClass +')').length;
+		}
+		else {
+			duplicates = $self.find(settings.rowSelector).length;
+		}
+		
+        var new_element    = $self.find(settings.rowSelector).clone();
 
-        // check data
-        if ( $.hasData(self) ) {
-            duplicates = parseInt($.data(self,'duplicates')) + 1;
-        }
-        else {
-            duplicates++;
-        }
-        
+        // set key
+        var duplicates_key = (parseInt($self.data('duplicates_key')) > 0) ? parseInt($self.data('duplicates_key')) : duplicates;
+		
         // check for maximum duplicates
-        if ( settings.maxDuplicates !== 0 && settings.maxDuplicates < duplicates ) { settings.limit(settings.maxDuplicates); return false; }
-        $.data(self, 'duplicates', duplicates);
-
-
+        if ( settings.maxDuplicates !== 0 && settings.maxDuplicates < duplicates ) { 
+				settings.limit(settings.maxDuplicates); 
+				return false; 
+		}
+		
 
         // remove super element id
-        new_element.attr('id','');
-
+        new_element.removeAttr('id');
+		
         // add to page and fade in
         new_element.prepend(settings.removeMarkup);
-        new_element.hide();
-        new_element.insertAfter(self);
+		new_element.html(new_element.html().replaceAll(settings.templateKey, '['+ duplicates_key + ']'));
+		new_element.hide();
+		
+		//remove the template class if this is a template
+		if (settings.templateClass != '') {
+			new_element.removeClass(settings.templateClass);
+		}
+		
+        new_element.appendTo(self);
         
         if ( settings.slideEffect ) {
-            
             new_element.css('opacity', 0.01);
             new_element.slideDown();
-        
         }
         
         new_element.fadeTo('fast', 1);
+		
+		$self.data('duplicates_key', ++duplicates_key);
         
         return false;
 
@@ -87,3 +112,18 @@
     return this;
   };
 })( jQuery );
+
+
+String.prototype.replaceAll = function(stringToFind,stringToReplace){
+
+	var temp  = this;
+	var index = temp.indexOf(stringToFind);
+
+	while(index != -1){
+		temp  = temp.replace(stringToFind,stringToReplace);
+		index = temp.indexOf(stringToFind);
+	}
+
+	return temp;
+
+}
